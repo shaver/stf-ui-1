7209057d33b61a1cafc314f3fa63320023d2e905
Mike Shaver <shaver@off.net>
2019-04-21 11:25:37 -0400

    Calculate current/max mass/crew/officers/craft.

--- src/ShipStatus.js
+++ src/ShipStatus.js
@@ -7,45 +7,76 @@ class ShipStatus extends Component {
     constructor(props) {
         super(props);
 
-        this.ship = props.ship;
-        this.classes = props.classes;
+        const { classes, shipType, components, GAME_DATA } = props;
+        this.state = { classes, shipType, components, GAME_DATA };
     }
 
-    renderStatusRow(label, current, max) {
+    renderStatusRow(i, label, current, max) {
         return (
-            <tr>
+            <tr key={i}>
                 <td>{label}:</td>
                 <td>{current}</td>
                 <td>/</td>
-                <td className={this.classes.statusMaxCell}>{max}</td>
+                <td className={this.state.classes.statusMaxCell}>{max}</td>
             </tr>
         )
     }
 
     renderStatusTable() {
-        const list = [];
-        
-        list.push(["Mass", 1500, 1800]);
-        list.push(["Crew", 24, 30]);
-        list.push(["Officers", 4, 5]);
+        const reduceComponents = (fn, start) => {
+            let current = start;
+    
+            const reduceWrapper = (f, prev, curr) => {
+                let after = f(prev, curr);
+                console.log("reduceComponents: ", prev, " + ", curr.name, " = ", after, " (",
+                    after - prev, ")");
+                return after;
+            }
 
+            for (let size of Object.values(this.state.components)) {
+                current = size.reduce((p, c) => reduceWrapper(fn, p, c), current);
+            }
+    
+            return current;
+        }
+    
+        const list = [];
+
+        let currentMass = reduceComponents((prev, curr) => prev + curr.mass, 0);
+        let maxMass = reduceComponents((prev, curr) => prev + curr.drive_mass, 0);
+        list.push(["Mass", currentMass, maxMass]);
+
+        let currentCrew = reduceComponents((prev, curr) => prev + curr.holds_crew, 0);
+        let maxCrew = this.state.GAME_DATA.SHIP_TYPES[this.state.shipType].max_crew;
+        list.push(["Crew", currentCrew, maxCrew]);
+
+        let currentOfficers = reduceComponents((prev, curr) => prev + curr.holds_officer, 0);
+        let maxOfficers = this.state.GAME_DATA.SHIP_TYPES[this.state.shipType].max_officer;
+        list.push(["Officers", currentOfficers, maxOfficers]);
+
+        let currentCraft = reduceComponents((prev, curr) => prev + curr.holds_craft, 0);
+        let maxCraft = this.state.GAME_DATA.SHIP_TYPES[this.state.shipType].max_craft;
+        list.push(["Craft", currentCraft, maxCraft]);
+        
         const rows = [];
-        for (let i of list) {
-            rows.push(this.renderStatusRow(i[0], i[1], i[2]));
+
+        for (let i = 0; i < list.length; i++) {
+            rows.push(this.renderStatusRow(i + 1, list[i][0], list[i][1], list[i][2]));
         }
 
         return (
             <table>
-                {rows}
+                <tbody>
+                    {rows}
+                </tbody>
             </table>
         );
     }
 
     render() {
-        const classes = this.classes;
         return (
-            <Grid item spacing={8}>
-                <Paper className={classes.statusTable}>
+            <Grid item>
+                <Paper className={this.state.classes.statusTable}>
                     {this.renderStatusTable()}
                 </Paper>
             </Grid>
